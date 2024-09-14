418ee134d2a8b56f961eda4d181fe1e4e0b22bb2
MarkCup <1586827710@qq.com>
2024-09-14 12:04:57 +0800

    finished coffee behaviour

---
+++ Assets/Framework/GameManager/CoffeeBehaviour.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CoffeeFramework
+{
+public class CoffeeBehaviour : MonoBehaviour
+{
+    //private
+    private void Awake()
+    {
+        Regist();
+    }
+
+    private void Regist()
+    {
+        //test whether functions are overrided
+        var type=GetType();
+        var updateMethodInfo=type.GetMethod("GameUpdate");
+        GameManager.GameUpdate updateMethod=null;
+        if(updateMethodInfo!=updateMethodInfo.GetBaseDefinition()){
+            updateMethod=GameUpdate; 
+        }
+        var fixedUpdateMethodInfo=type.GetMethod("GameFixedUpdate");
+        GameManager.GameFixedUpdate fixedUpdateMethod=null;
+        if(fixedUpdateMethodInfo!=fixedUpdateMethodInfo.GetBaseDefinition()){
+            fixedUpdateMethod=GameFixedUpdate; 
+        }
+        var pauseMethodInfo=type.GetMethod("GamePause");
+        GameManager.GamePause pauseMethod=null;
+        if(pauseMethodInfo!=pauseMethodInfo.GetBaseDefinition()){
+            pauseMethod=GamePause; 
+        }
+
+        GameManager.RegistBehaviour(updateMethod,fixedUpdateMethod,pauseMethod);
+    }
+
+    protected virtual void GameUpdate(float time)
+    {
+
+    }
+
+    protected virtual void GameFixedUpdate(float time)
+    {
+
+    }
+
+    protected virtual void GamePause(bool isPause)
+    {
+
+    }
+}
+}
--- Assets/Framework/GameManager/GameManager.cs
+++ Assets/Framework/GameManager/GameManager.cs
@@ -1,66 +1,165 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build.Content;
 using UnityEngine;
 
-/// <summary>
-/// Sigleton GameManager
-/// </summary>
-public class GameManager : MonoBehaviour
+namespace CoffeeFramework
 {
-    //public
-    public static GameManager Instance { get; private set; }
-
-    public bool replaceOldGameManager = false;
-
-    public float fixedUpdateTimeGap = 0.02f;
-
-    public delegate void GameUpdate(float time);
-    public event GameUpdate OnUpdate;
-    public delegate void GameFixedUpdate(float time);
-    public event GameFixedUpdate OnFixedUpdate;
-
-    public delegate void Pause(bool isPause);
-    public event GameUpdate OnPauseChange;
-
-    protected void Destroy()
+    /// <summary>
+    /// Sigleton GameManager
+    /// </summary>
+    public class GameManager : MonoBehaviour
     {
-        Debug.Log("Old game manager is being destroying.");
-        Destroy(gameObject);
-    }
+        //public
 
-    //private
-    private void Awake()
-    {
-        if (Instance != null)
+        public static GameManager Instance { get; private set; }
+
+        /// <summary>
+        /// To regist a coffee behaciour object
+        /// </summary>
+        /// <param name="update"></param>
+        /// <param name="fixedUpdate"></param>
+        public static void RegistBehaviour(GameUpdate update = null, GameFixedUpdate fixedUpdate = null, GamePause pause = null)
         {
-            if (replaceOldGameManager)
+            if (Instance)
             {
-                Debug.Log("Destroying old game manager.");
-                Instance.Destroy();
-                Instance = this;
+                if (update != null)
+                {
+                    Instance.OnUpdate += update;
+                }
+                if (fixedUpdate != null)
+                {
+                    Instance.OnFixedUpdate += fixedUpdate;
+                }
             }
             else
             {
-                Debug.Log("There are already have a GameManager.");
-                Destroy();
+
+                holdRegists.Add((update, fixedUpdate, pause));
             }
         }
-        else
+
+        /// <summary>
+        /// whether to replace old exist game manager
+        /// </summary>
+        public bool replace = false;
+
+        /// <summary>
+        /// whether to inherit gameobjects from old game manager when replacing    
+        /// </summary>
+        public bool inherit = false;
+
+        /// <summary>
+        /// the time between tow fixed updates
+        /// </summary>
+        public float fixedUpdateTimeGap = 0.02f;
+
+        /// <summary>
+        /// Game Update delegate
+        /// </summary>
+        /// <param name="time">as same as Time.deltaTime</param>
+        public delegate void GameUpdate(float time);
+
+        /// <summary>
+        /// Game Fixed Update delegate
+        /// </summary>
+        /// <param name="time">the fixed update gap time</param>
+        public delegate void GameFixedUpdate(float time);
+
+        /// <summary>
+        /// pause delegate
+        /// </summary>
+        /// <param name="isPause"></param>
+        public delegate void GamePause(bool isPause);
+
+        //private
+
+        protected void Destroy()
         {
-            Instance = this;
+            Debug.Log("Old game manager is being destroying.");
+            Destroy(gameObject);
+        }
+
+        private static List<(GameUpdate, GameFixedUpdate, GamePause)> holdRegists = new();
+
+        private event GameUpdate OnUpdate;
+
+        public event GameFixedUpdate OnFixedUpdate;
+
+        public event GamePause OnPauseChange;
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                if (replace)
+                {
+                    Debug.Log("Destroying old game manager.");
+                    Init(Instance);
+                    Instance.Destroy();
+                    Instance = this;
+                }
+                else
+                {
+                    Debug.Log("There are already have a GameManager.");
+                    Destroy();
+                }
+            }
+            else
+            {
+                Instance = this;
+            }
+        }
+
+        private void Init()
+        {
+            HandleHold();
+        }
+
+        private void Init(GameManager oldManager)
+        {
+            foreach (var update in oldManager.OnUpdate.GetInvocationList())
+            {
+                OnUpdate += (GameUpdate)update;
+            }
+            foreach (var fixdeUpdate in oldManager.OnFixedUpdate.GetInvocationList())
+            {
+                OnFixedUpdate += (GameFixedUpdate)fixdeUpdate;
+            }
+            HandleHold();
+        }
+
+        private void HandleHold()
+        {
+            foreach ((var update, var fixedUpdate, var pause) in holdRegists)
+            {
+                if (update != null)
+                {
+                    OnUpdate += update;
+                }
+                if (fixedUpdate != null)
+                {
+                    OnFixedUpdate += fixedUpdate;
+                }
+                if (pause != null)
+                {
+                    OnPauseChange += pause;
+                }
+            }
+        }
+
+        private float timmer = 0;
+
+        private void Update()
+        {
+            timmer += Time.deltaTime;
+            while (timmer > fixedUpdateTimeGap)
+            {
+                timmer -= fixedUpdateTimeGap;
+                OnFixedUpdate.Invoke(fixedUpdateTimeGap);
+            }
+            OnUpdate.Invoke(Time.deltaTime);
         }
     }
 
-    private float timmer = 0;
-
-    private void Update()
-    {
-        timmer += Time.deltaTime;
-        while (timmer > fixedUpdateTimeGap)
-        {
-            timmer -= fixedUpdateTimeGap;
-            OnFixedUpdate.Invoke(fixedUpdateTimeGap);
-        }
-        OnUpdate.Invoke(Time.deltaTime);
-    }
 }
